a732094aea83a5e1b107a3fe81008335ad733308
JohnTebou <johntebou@gmail.com>
2025-03-13 21:54:34 -0400

    b search

--- binary_search_experimentation.py
+++ binary_search_experimentation.py
@@ -19,3 +19,19 @@ def findpeak(arr):
 
 print(findpeak(arr1))
 print(findpeak(arr2))
+
+# don't use ceiling/floor to calculate middle if not working
+# with indices or necessarily integral values
+def findsqrt(x):
+	l = 0
+	r = -(-51*x // 100) # ceiling of 1/2*x plus some small constant
+	tolerance = 1e-15 # error
+	while r - l >= tolerance:
+		m = l + (r - l) / 2
+		if m*m > x:
+			r = m
+		else:
+			l = m
+	return l + (r - l) / 2
+
+print(findsqrt(2))
